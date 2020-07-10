e7c1c24a0691ebf99d04c38430b222d0ce671eee
Arvin Alvea <arvinalvea@gmail.com>
2020-07-10 09:31:17 -0400

    Added "See Projects" & "Back To Top" buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+/* ============================================= */
+/*              Global Variables                 */
+/* ============================================= */
+
+const projectBtn = document.querySelector('#projectBtn');
+
+
+/* ============================================= */
+/*                     Main                      */
+/* ============================================= */
+
+// Hide "See Projects" on load up
+window.addEventListener('load', (event) => {
+    projectBtn.classList = 'hidden';
+});
+
+// Smooth scroll when arrow button is clicked
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -5,5 +22,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+        projectBtn.classList = "col-lg-12";
     });
 });
+
+
+
+
